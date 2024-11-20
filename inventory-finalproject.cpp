3fe1c144098dcd892b0cb63e7cfb19f53962b834
jsariol <jsariol@addu.edu.ph>
2024-11-20 13:42:57 +0800

    Update inventory-finalproject.cpp

--- inventory-finalproject.cpp
+++ inventory-finalproject.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    //placeholder shit for shit.
+    cout << "Welcome to Inventory Management System" << endl;
+    cout << "--------------------------------------" << endl;
+    cout << "1. Add New Item" << endl;
+    cout << "2. View All Items" << endl;
+    cout << "3. Update Item Quantity" << endl;
+    cout << "4. Remove an Item" << endl;
+    cout << "5. Generate Stock Report" << endl;
+    cout << "6. Save & Exit" << endl;
     return 0;
 }

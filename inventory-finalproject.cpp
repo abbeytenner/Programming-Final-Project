852e09f5f58b4c61cecb794f647e4345dff5357b
LMatobato <lmatobato@addu.edu.ph>
2024-11-25 13:19:23 +0800

    Update inventory-finalproject.cpp

--- inventory-finalproject.cpp
+++ inventory-finalproject.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+#include <limits>
 
 using namespace std;
 
-int main()
-{
-    char choiceInput;
-    cout << "Welcome to Inventory Management System" << endl;
+void menu() {
+	cout << "Welcome to Inventory Management System" << endl;
     cout << "--------------------------------------" << endl;
     cout << "1. Add New Item" << endl;
     cout << "2. View All Items" << endl;
@@ -14,31 +19,53 @@ int main()
     cout << "5. Generate Stock Report" << endl;
     cout << "6. Save & Exit" << endl;
     cout << "Enter choice: " <<endl;
+}
+
+int main()
+{
+    char choiceInput;
+    menu();
     cin >> choiceInput;
-    
+    switch (choiceInput){
+    	case '1':
+    		addItem();
+    		break;
+    	case '2':
+    		addItem();
+    		break;
+    	case '3':
+    		addItem();
+    		break;
+    	case '4':
+    		addItem();
+    		break;
+    	case '5':
+    		addItem();
+    		break;
+    	case '6':
+    		addItem();
+    		break;
+    	
+    }
     return 0;
 }
 
-addItem() {
+void addItem() {
     
 }
 
+void viewItems() {
+						 
+}
 
-viewItems() {
+void updateQuantity() {
     
 }
 
-
-updateQuantity() {
+void removeItem() {
     
 }
 
-
-removeItem() {
-    
-}
-
-
-generateReport() {
+void generateReport() {
     
 }

da0205d1ccde51e3ba1f330994399fec09a9d957
LMatobato <lmatobato@addu.edu.ph>
2024-11-25 13:01:34 +0800

    Update inventory-finalproject.cpp
    added fucntions and the txt

--- inventory-finalproject.cpp
+++ inventory-finalproject.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    char choiceInput;
     cout << "Welcome to Inventory Management System" << endl;
     cout << "--------------------------------------" << endl;
     cout << "1. Add New Item" << endl;
@@ -12,5 +13,32 @@ int main()
     cout << "4. Remove an Item" << endl;
     cout << "5. Generate Stock Report" << endl;
     cout << "6. Save & Exit" << endl;
+    cout << "Enter choice: " <<endl;
+    cin >> choiceInput;
+    
     return 0;
+}
+
+addItem() {
+    
+}
+
+
+viewItems() {
+    
+}
+
+
+updateQuantity() {
+    
+}
+
+
+removeItem() {
+    
+}
+
+
+generateReport() {
+    
 }

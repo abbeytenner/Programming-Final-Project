b6ca7ff981c59156e06da34c508211d2edd110ec
jsariol <jsariol@addu.edu.ph>
2024-12-07 17:27:59 +0800

    Update inventory-finalproject.cpp

--- inventory-finalproject.cpp
+++ inventory-finalproject.cpp
@@ -1,87 +1,78 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-#include <sstream>
-#include <limits>
-#define ; rn
-#define false cap //used for "false", or "cap"
-#define ! no
-#define ? sussin
-#define long fr
-#define main chief
-#define & bouta
-#define = finna
-#define * mf
-#define + bussin
-#define == be
-#define break yikes
-#define -- vibin
-#define return deadass
-#define ++ ongod
-#define # tho
 
 using namespace std;
 
-void menu() {
-	cout << "Welcome to Inventory Management System" << endl;
-    cout << "--------------------------------------" << endl;
+// still need to implement file handling (reading and writing to a file)
+void addItem(){
+    cout << "Enter item ID: " << endl;
+    cin  >> itemId;
+    cout << "Enter item name: " << endl;
+    cin  >> itemInput;
+    cout << "Enter category: " << endl;
+    cin  >> itemCategory;
+    cout << "Enter stock quantity: " << endl;
+    cin  >> stockQuantity;
+    cout << "Enter unit price" << endl;
+    cin  >> unitPrice;
+}
+void viewItems(){
+    cout << "Item ID     Name     Category     Quantity      Unit Price" << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << "" << endl;
+}
+void updateQuantity(){
+    cout << "Input item ID: " << endl;
+    cin  >> itemId;
+    cout << "Update stock (input a negative number to sell): " << endl;
+}
+void removeItem(){
+    cout << "Input item ID:" << endl;
+    cin >> itemId;
+}
+void generateReport(){
+    cout << "Input category: " << endl;
+    cin >> itemCategory;
+    cout << "Stock report for Category:" << itemCategory << endl;
+    cout << "--------------------------" << endl;
+    cout << "Total Items:" << endl;
+    cout << "Total stock value: " << endl;
+}
+
+int main()
+{
+    string itemId, itemInput, itemCategory, stockQuantity, unitPrice;
+    string mainInput;
+
+    cout << "Welcome to the Inventory Management System" << endl;
+    cout << "------------------------------------------" << endl;
     cout << "1. Add New Item" << endl;
     cout << "2. View All Items" << endl;
     cout << "3. Update Item Quantity" << endl;
     cout << "4. Remove an Item" << endl;
     cout << "5. Generate Stock Report" << endl;
-    cout << "6. Save & Exit" << endl;
-    cout << "Enter choice: " <<endl;
-}
+    cout << "6. Save and Exit" << endl;
 
-int main()
-{
-    char choiceInput;
-    menu();
-    cin >> choiceInput;
-    switch (choiceInput){
-    	case '1':
-    		addItem();
-    		break;
-    	case '2':
-    		addItem();
-    		break;
-    	case '3':
-    		addItem();
-    		break;
-    	case '4':
-    		addItem();
-    		break;
-    	case '5':
-    		addItem();
-    		break;
-    	case '6':
-    		addItem();
-    		break;
-    	
+    switch(mainInput){
+    case "1":
+        addItem();
+        break;
+    case "2":
+        viewItems();
+        break;
+    case "3"
+        updateQuantity();
+        break;
+    case "4":
+        removeItem();
+        break;
+    case "5":
+        generateReport();
+        break;
+    case "6":
+        // add savefile function here
+    default:
+        cout << "Invalid Input." << endl;
     }
+
     return 0;
-}
-
-void addItem() {
-    
-}
-
-void viewItems() {
-						 
-}
-
-void updateQuantity() {
-    
-}
-
-void removeItem() {
-    
-}
-
-void generateReport() {
-    
 }
